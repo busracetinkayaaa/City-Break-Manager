72776cda7c632079e2dfbd00d839216760d1a542
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2025-12-24 17:35:49 +0300

    Initiate the project

---
+++ src/main/java/com/example/CityBreakManager/CityBreakManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CityBreakManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBreakManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBreakManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CityBreakManager/CityBreakManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CityBreakManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBreakManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
